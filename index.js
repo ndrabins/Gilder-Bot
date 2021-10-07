ade695b1e9783b78932caeb197e5bd228f2aa3ca
Noah Drabinsky <ndrabins@users.noreply.github.com>
2021-10-07 16:57:06 +0000

    Add connection to supabase

--- index.js
+++ index.js
@@ -4,9 +4,42 @@ const keepAlive = require("./server")
 const Database = require("@replit/database")
 
 const db = new Database()
-const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MEMBERS] });
+const client = new Client({ 
+  intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MEMBERS],
+  partials: ['MESSAGE', 'REACTION', 'CHANNEL']
+});
+
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env['SUPABASE_URL']
+const supabaseAnonKey = process.env["SUPABASE_ANON_KEY"]
+// Supabase for confirming connection
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+async function tryInsert() {
+// Insert member 
+// This happens on guild member verifying wallet + discord
+// const { data, error } = await supabase
+//   .from('guild_member')
+//   .insert(
+//     { id: '123423', wallet_key: "12312", guild_id: '12364' }
+//   );
+
+// Fetch members by guild id
+// used on frontend for adding wallets, distributing coins
+  const { data, error } = await supabase
+    .from('guild_member')
+    .select()
+    .eq('guild_id', "123")
+
+  console.log('Inserting into supabase', data, error);
+};
 
 const GILD_DAO_ROLE = "Gild DAO"
+
+// 1. On ready, create new channel
+// 2. Bot posts URL link for guild users to add wallet/ sign up for DAO
+// 3. After DAO creation, post another message in channel with emoji for getting DAO role in discord server
 
 function createGilderRole(guild) {
   guild.roles.create({
@@ -17,6 +50,11 @@ function createGilderRole(guild) {
     },
     reason: 'For people who are in gild',
   });
+}
+
+function getGuilds(msg) {
+  console.log(msg.guild.members.cache);
+  return msg.guild.members.cache;
 }
 
 function addGilderRole (interaction) {
@@ -30,6 +68,8 @@ function addGilderRole (interaction) {
   }
 }
 
+
+// Temp for messaging
 client.on("message", msg => {
   if (msg.author.bot) return
 
@@ -42,7 +82,19 @@ client.on("message", msg => {
     msg.channel.send(`Creating role ${GILD_DAO_ROLE}.`);
     createGilderRole(msg.guild);
   } 
-})
+
+  if (msg.content.startsWith("$getGuilds")) {
+    msg.channel.send(`Getting Guilds.`);
+    getGuilds(msg);
+  } 
+});
+
+// TODO: Add roles on reaction after checking if they are authenticated w/ wallet
+// https://www.smashingmagazine.com/2021/02/building-discord-bot-discordjs/
+client.on('messageReactionAdd', async (reaction, user) => {});
+
+// Make call to our DAO service to see if users wallet has correct tokens
+function checkIfUserInDAO (interaction) {}
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
